a1016effcb5b899cd470165a8c7d21f20ce5a246
RandomNick7 <dorin.otgon@gmail.com>
2026-05-28 18:01:22 +0300

    Text: Message Change

--- src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
+++ src/main/kotlin/md/utm/cloudapp/rest/MainController.kt
@@ -8,6 +8,6 @@ class MainController {
 
     @GetMapping("/")
     fun main(): String {
-        return "Placeholder for a landing page..."
+        return "Placeholder for a landing page... 00"
     }
 }
